de9754e3ef3f306205ec05ce4a8c7a965f713ca5
Alex Sink <alexsink@yahoo.com>
2023-05-10 10:54:19 -0700

    extra validation and correctness

--- ghrelease.sh
+++ ghrelease.sh
@@ -2,13 +2,16 @@
 outputdirectorybase="essence_combined_packages/"
 
 function get_version_numbers {
-    directory_to_look_for_tgz_files=$1
-    #local -n version_numbers=$2             # use nameref for indirection
+    directory_to_look_for_tgz_files="$1"
     local version_numbers=()
-    # Loop through all the tgz files in the directory_to_look_for_tgz_files and get version numbers
+
+    # check if the directory exists
+    if [[ ! -d $directory_to_look_for_tgz_files ]]; then
+        echo "Directory $directory_to_look_for_tgz_files does not exist"
+        return 1
+    fi
 
     for file in "$directory_to_look_for_tgz_files"*.tgz; do
-        # Check if the file is actually a tgz file
         if [ ! -f "$file" ]; then
             printf "Skipping $file: not a regular file\n" >&2
             continue
@@ -27,34 +30,44 @@ function get_version_numbers {
             continue
         fi
     done
-    echo "${version_numbers[@]}" # Return the array elements
+    echo "${version_numbers[@]}"
 }
 
-#populate version numbers based on versions seen in tgz files
-
 tgzdirectory="com.htc.upm.wave.essence/"
-versions=($(get_version_numbers $tgzdirectory))
-cd $outputdirectorybase
 
-# Loop through the versions array
+# check if the directory exists
+if [[ ! -d $tgzdirectory ]]; then
+    echo "Directory $tgzdirectory does not exist"
+    exit 1
+fi
+
+versions=($(get_version_numbers "$tgzdirectory"))
+
+# check if the directory exists
+if [[ ! -d $outputdirectorybase ]]; then
+    echo "Directory $outputdirectorybase does not exist"
+    exit 1
+fi
+
+cd "$outputdirectorybase"
+
 for version in "${versions[@]}"
 do
-    # Create a new release on GitHub for the current version
-    winpty gh release create $version -t versions/$version -n ""
+    winpty gh release create "$version" -t "versions/$version" -n ""
 
-    # Create a tgz file of the current version
-    #tar -czvf $version.tar.gz $version/
     package_names=("com.htc.upm.wave.xrsdk" "com.htc.upm.wave.native"  "com.htc.upm.wave.essence" )
     
     tgzFileString=""
-    for package in ${package_names[@]};
+    for package in "${package_names[@]}";
     do
-        tgzfile=../$package/$package-$version.tgz
-        #echo "tgzFiles: ($tgzFileString)"
-        #gh release upload $version $version.tar.gz
-        tgzFileString+="$tgzfile " 
+        tgzfile="../$package/$package-$version.tgz"
+
+        if [[ ! -f $tgzfile ]]; then
+            echo "File $tgzfile does not exist"
+            continue
+        fi
+
+        tgzFileString+="$tgzfile "
     done
-    # Upload the tgz file to the GitHub release for the current version
-    #gh release upload $version $version.tar.gz
-    winpty gh release upload versions/$version $tgzFileString
+    winpty gh release upload "versions/$version" "$tgzFileString"
 done
