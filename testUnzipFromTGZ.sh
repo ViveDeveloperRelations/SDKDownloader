7019af906cdba31fd5fc9d56b64a50e63d57efdf
Alex Sink <alexsink@yahoo.com>
2023-05-15 11:15:40 -0700

    update and start to have a code path for merged branch (#2)
    * test combined pages and also force psuh to allow faster iteration
    
    * add paths to packages
    
    * delete meta folders and prevent the extra "package" directory
    
    * hardcode to only build test repo against 5.2.1-r.1
    
    * quoting rules and functions to keep similar logic together/aka cohesion
    
    * fix: remove duplicate checkin
    
    * ignore combined tgz files since they're generated
    
    * re-checkout master at end of loop for sanity and also tgz file that should not need to incude an extra directory
    
    * if it existed before, ignore preivous patching
    
    * add in patch for wavexr settings and filepatsh
    
    * change meta
    
    * reference point for metas
    
    * new meta for license
    
    * add the other meta file
    
    * test patch
    
    * fix merge issue in patch
    
    * fix build dependencies as well
    
    * fix extra packages folder ref
    
    * fix build again
    
    * path seperator neededd
    
    * don't add the meta files in the patch, add them in the build process
    
    * meta files for the sub-package folders
    
    * revert to only indiivaul so i cna rebuild iwhtout that package directory
    
    * use all version numbers again

--- testUnzipFromTGZ.sh
+++ testUnzipFromTGZ.sh
@@ -5,9 +5,10 @@
 outputdirectorybase="essence_combined_packages/"
 
 function setup_git_repo {
-    local repo_base=$1
+    local repo_base="$1"
     mkdir -p "$repo_base"
-    rm -rf $repo_base/.git
+    rm -rf "$repo_base/.git"
+    rm -rf "$repo_base"/*
 
     # Remove any existing non-Git files in the output directory
     #find "$repo_base" -mindepth 1 -maxdepth 1 ! -name '.*' -delete
@@ -26,11 +27,10 @@ function setup_git_repo {
     git -C "$repo_base" commit -am "initial commit"
     sleep 1
 }
-setup_git_repo $outputdirectorybase
+setup_git_repo "$outputdirectorybase"
 
 function get_version_numbers {
-    directory_to_look_for_tgz_files=$1
-    #local -n version_numbers=$2             # use nameref for indirection
+    directory_to_look_for_tgz_files="$1"
     local version_numbers=()
     # Loop through all the tgz files in the directory_to_look_for_tgz_files and get version numbers
 
@@ -61,7 +61,6 @@ function get_version_numbers {
 
 tgzdirectory="com.htc.upm.wave.essence/"
 version_numbers=($(get_version_numbers $tgzdirectory))
-#version_numbers=()
 
 # extract packages from their downloaded locations to a git repo and check them in
 function extract_packages {
@@ -72,7 +71,7 @@ function extract_packages {
     local packages=("$@")    # Now $@ contains the remaining parameters
 
     # Unzip the tgz file into the output directory
-    for package in ${packages[@]};
+    for package in "${packages[@]}";
     do
         local tgzfile=$package/$package-$version.tgz
         local package_output_directory=$outputdirectory$package
@@ -80,21 +79,64 @@ function extract_packages {
         rm -rf "$package_output_directory"
         mkdir -p "$package_output_directory"
         printf "extracting $tgzfile to $package_output_directory\n" >&2
-        tar -xzf "$tgzfile" -C "$package_output_directory/"
+        tar -xzf "$tgzfile" --strip-components=1 -C "$package_output_directory/"
     done
 
-    #for testing the single-package setup working for now
-    find "$outputdirectory/" -name "package.json" -type f -delete
-    echo "all files in packages"
-    ls files_to_copy_to_repo/packag*
-    cp files_to_copy_to_repo/packag* "$outputdirectory/"
-
-
+}
+function commit_main_branch {
+    local outputdirectory="$1"
+    local version="$2"
+    mainbranch="master"
+    git -C "$outputdirectory" checkout "$mainbranch"
     # Add all files to the Git repository and create a new commit
     git -C "$outputdirectory" add -A
     git -C "$outputdirectory" commit -am "version $version"
     # Tag the commit with the version number
     git -C "$outputdirectory" tag -a "versions/$version" -m "version $version"
+}
+function patch_for_combined_package {
+    local outputdirectory="$1"
+
+    find "$outputdirectory" -name "package.json" -type f -delete
+    find "$outputdirectory" -name "package.json.meta" -type f -delete
+    echo "all files in packages"
+    ls files_to_copy_to_repo/packag*
+    cp files_to_copy_to_repo/packag* "$outputdirectory/"
+    cp files_to_copy_to_repo/com.htc*meta "$outputdirectory/"
+}
+function commit_combined_branch {
+    local outputdirectory="$1"
+    local version="$2"
+
+    combinedbranch="combined"
+    combinedpackagename="com.htc.upm.wave.nativecombined"
+
+    #create branch if branch not created in git
+    if git rev-parse --verify --quiet "$combinedbranch" >/dev/null; then
+        git -C "$outputdirectory" checkout "$combinedbranch"
+        #Reset the branch to the previous commit, ignore the previous patch
+        git reset --hard HEAD~1
+    else
+        # Create a new branch and switch to it
+        git -C "$outputdirectory" checkout -b "$combinedbranch"
+    fi
+    
+    #apply patch if needed
+    #if patch file exists, apply it
+    patchfile=files_to_copy_to_repo/patch_$version.diff
+    if [ -f $patchfile ]; then
+        echo "applying patch for $version"
+        git -C "$outputdirectory" apply ../$patchfile
+    fi
+    git -C "$outputdirectory" add -A
+    git -C "$outputdirectory" commit -am "patches for creating combined version $version"
+    # Tag the commit with the version number
+    git -C "$outputdirectory" tag -a "combined/$version" -m "combined version $version"
+    #create a tgz file of the combined output for publishing purposes
+    #create directory for tgz files if it doesn't exist
+    mkdir -p "$combinedpackagename"
+    echo "tar -czf "$combinedpackagename/$combinedpackagename-$version.tgz" -C "$outputdirectory" --exclude=".git" --exclude=".gitignore" ."
+    tar -czf "$combinedpackagename/$combinedpackagename-$version.tgz" -C "$outputdirectory" --exclude=".git" --exclude=".gitignore" .
 }
 
 
@@ -103,12 +145,23 @@ for version in "${version_numbers[@]}"; do
     echo " version $version"
 
     extract_packages $outputdirectorybase $version "${package_names[@]}" 
+    commit_main_branch $outputdirectorybase $version
+    #patch_for_combined_package $outputdirectorybase
+    #commit_combined_branch $outputdirectorybase $version
+
+    mainbranch="master"
+    #git -C "$outputdirectorybase" checkout "$mainbranch"
 done
 
-#origin=https://github.com/ViveDeveloperRelations/WaveNativeIndividual.git
-origin=https://github.com/ViveDeveloperRelations/WaveCombined.git
-git -C "$outputdirectorybase" remote add origin $origin
-git -C "$outputdirectorybase" push origin master
-#https://github.com/ViveDeveloperRelations/WaveNativeIndividual.git
-git -C "$outputdirectorybase" push --tags
 
+function push_to_remote {
+    local outputdirectorybase="$1"
+    origin=https://github.com/ViveDeveloperRelations/WaveNativeIndividual.git
+    #origin=https://github.com/ViveDeveloperRelations/WaveCombined_test.git
+    git -C "$outputdirectorybase" remote add origin $origin
+    git -C "$outputdirectorybase" push origin master --force
+    #https://github.com/ViveDeveloperRelations/WaveNativeIndividual.git
+    git -C "$outputdirectorybase" push --tags --force
+}
+
+push_to_remote "$outputdirectorybase"
